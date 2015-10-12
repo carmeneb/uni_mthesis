cc0078b898d9847e7b41a83cc1a567e8a5b0590e
Carmen Baillie <cbaillie@outlook.com>
2015-10-12 14:47:10 +1000

    removed redundant files and renamed files

--- MongoDB/search_functions.js
+++ MongoDB/search_functions.js
@@ -1,12 +1,25 @@
 
+// Searching for text in fields
+
+/// Create an index where to search for text
 
 db.profiles.createIndex( { "details.dev_blurb" : "text" } )
 
-
-19:03pm
-
-Aggregation: Text Search $match (stage 1)
+/// Search for text  (example in aggregation pipeline)
 
 {
-	$text: { $search: "expert" }
+	$match: {
+					$text: { $search: "expert" }
+					}
+}
+
+// Profiles with Assignment Details
+
+{
+	$match: {
+	$or: [ { "details.dev_billed_assignments": { $gt: 0.0 } }
+					, { "details.dev_total_hours": { $gt: 0.0 } }
+					, { "details.assignments.fp.job": { $exists: true } }
+					, { "details.assignments.hr.job": { $exists: true } } ]
+	}
 }
